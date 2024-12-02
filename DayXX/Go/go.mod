f8c5b5ad4a02a1c6cfdfb20f83ba19cb37d907b1
Miloš Rackov <milos@rackov.ch>
2024-12-02 22:59:57 +0100

    Create Go template

---
+++ DayXX/Go/go.mod
@@ -0,0 +1,3 @@
+module AoC2024DXX
+
+go 1.23.3
---
+++ DayXX/Go/part1/part1.go
@@ -0,0 +1,9 @@
+// Copyright (c) Miloš Rackov 2024
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+package part1
+
+func Part1(input []string) int {
+	return 0
+}
